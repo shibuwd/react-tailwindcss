69ecf9308a8cc000962631eb726abe99e92246d1
shibuwd <shibusarkar619@gmail.com>
2022-05-12 14:52:44 +0600

    react-tailwindcss

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <h1 className="font-Poppins font-bold text-3xl text-green-900 ">Hello world!</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                // using custom font familly.
+                Poppins: ['Poppins', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};

6934d4fe6fe28034c39130a9da955de603bf7319
shibuwd <shibusarkar619@gmail.com>
2022-05-13 21:50:57 +0600

    package update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-    return <h1 className="font-Poppins font-bold text-3xl text-green-900 ">Hello world!</h1>;
+    return <h1 className="font-Poppins font-bold text-3xl text-red-700 ">Hello world!</h1>;
 }
 
 export default App;
